924bd4fee6bcceabd55022e43b982569635d21ec
UNIDY2002 <UNIDY2002@outlook.com>
2022-12-29 18:43:38 +0800

    Update template

--- Makefile
+++
@@ -1,6 +0,0 @@
-.PHONY: main
-
-all: main
-
-main:
-	g++ -O2 main.cpp Judge.cpp Strategy.cpp -o main
--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+#ifdef ONLINE_JUDGE
+
 #include <cstring>
 #include <cstdio>
 #include "Point.h"
@@ -16,7 +18,6 @@ int main()
     int row, col;
     int* board; //chess board
     int* top; //available position
-    send("ok"); // DO NOT REMOVE THIS LINE!
 
     scanf("%d %d %d %d", &row, &col, &noX, &noY);
     board = new int[row * col];
@@ -24,18 +25,10 @@ int main()
     while(true)
     {
         scanf("%d %d", &lastX, &lastY);
-        int top_size, board_size;
-        scanf("%d", &top_size);
-        for(int i = 0; i < top_size; i ++) scanf("%d", &top[i]);
-        scanf("%d", &board_size);
-        for(int i = 0; i < board_size; i ++) scanf("%d", &board[i]);
+        for(int i = 0; i < col; i ++) scanf("%d", &top[i]);
+        for(int i = 0; i < row * col; i ++) scanf("%d", &board[i]);
 
         Point* point = getPoint(row, col, top, board, lastX, lastY, noX, noY);
-        if(point == nullptr)
-        {
-            send("-2 -2");
-            break;
-        }
         char msg[6];
         sprintf(msg, "%d %d", point->x, point->y);
         send(msg);
@@ -43,3 +36,5 @@ int main()
     }
     return 0;
 }
+
+#endif  // ONLINE_JUDGE
